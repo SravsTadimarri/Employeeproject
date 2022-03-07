8d713e5dd2506576d6d0fb98794f868dfa8b160d
SravaniTadimarri <sravanitadimarri84@gmail.com>
2022-03-07 04:34:50 +0000

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(100,'pooja','kaur');
+insert into employee (id, name, last_name) 
+values(101,'kavitha' ,'reddy' );
+insert into employee(id, name, last_name ) 
+values(102,'keerthi', 'rao');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
